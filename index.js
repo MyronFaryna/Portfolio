813c834ebfe34999e355aa6b3ee2808f2ce15447
MyronFaryna <miron11fa@gmail.com>
2025-03-24 16:11:06 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+
+window.onload = function() {
+    var item = document.querySelector('.slide-right');
+    item.style.transform = 'translateX(0)'; 
+    item.style.opacity = '1'; 
+  };
+
+  $("#1").click(function(){
+    window.location.href = "https://myronfaryna.github.io/Drums/";
+  })
+
+  $("#2").click(function(){
+    window.location.href = "https://myronfaryna.github.io/Dice_Game/";
+  })
+
+  $("#3").click(function(){
+    window.location.href = "https://myronfaryna.github.io/TinDog/";
+  })
+
+  $("#4").click(function(){
+    window.location.href = "https://myronfaryna.github.io/Simon-Game/";
+  })
+
+  
